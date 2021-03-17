39b767ef53fc3fb88754528a0ce222662c06ba30
elpaablo <donopwhile1@gmail.com>
2021-03-17 11:11:29 +0000

    Launcher3: Move restart check to home page
    It's just a flag check. And avoids having to implement a listener for the home key (and still miss home gesture).
    Also, restarting the launcher when navigating to previous settings page doesn't seem right.
    This way, the launcher is restarted once, if needed, when user goes back to home page, which is where the changes are visible, anyway.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1206,6 +1206,8 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityResumed(this);
         }
+                        
+        LauncherAppState.getInstanceNoCreate().checkIfRestartNeeded();
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection(traceToken);
