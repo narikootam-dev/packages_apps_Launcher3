2e1504156dff33c8f0095901d77928429820d176
Fran Palmero <sweetydesigns@gmail.com>
2022-07-28 02:22:41 +0200

    Launcher3: Display memory info above overview actions
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -272,7 +272,9 @@ public class DeviceProfile {
     public int flingToDeleteThresholdVelocity;
 
     // Meminfo in overview
-    public int memInfoMarginPx;
+    public int memInfoMarginGesturePx;
+    public int memInfoMarginThreeButtonPx;
+    public int memInfoMarginTop;
 
     /** TODO: Once we fully migrate to staged split, remove "isMultiWindowMode" */
     DeviceProfile(Context context, InvariantDeviceProfile inv, Info info, WindowBounds windowBounds,
@@ -524,8 +526,12 @@ public class DeviceProfile {
 
         splitPlaceholderInset = res.getDimensionPixelSize(R.dimen.split_placeholder_inset);
 
-        memInfoMarginPx = res.getDimensionPixelSize(
-                R.dimen.meminfo_bottom_margin);
+        memInfoMarginGesturePx = res.getDimensionPixelSize(
+                R.dimen.meminfo_bottom_margin_gesture);
+        memInfoMarginThreeButtonPx = res.getDimensionPixelSize(
+                R.dimen.meminfo_bottom_margin_three_button);
+        memInfoMarginTop = res.getDimensionPixelSize(
+                R.dimen.meminfo_top_margin);
 
         // Calculate all of the remaining variables.
         extraSpace = updateAvailableDimensions(res);
